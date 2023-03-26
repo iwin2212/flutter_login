7827ddbaa940183abe79e80e04bb5bc13c1c97f9
Thangnb <nbthang2212@gmail.com>
2023-03-26 21:30:58 +0700

    check login app

---
+++ lib/splash/splash.dart
@@ -0,0 +1,2 @@
+// The [splash] feature will just contain a simple view which will be rendered right when the app is launched while the app determines whether the user is authenticated.
+export 'view/splash_page.dart';
